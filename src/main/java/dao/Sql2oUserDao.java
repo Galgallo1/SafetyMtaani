7744844500509afe47bc1f5eaaf2456c07c93555
Guyoguyes <g.abuba43@gmail.com>
2020-06-16 02:09:43 +0300

    added database.sql contains db structure for user
    done dao for add and getAll for UserDao class

---
+++ src/main/java/dao/Sql2oUserDao.java
@@ -0,0 +1,36 @@
+package dao;
+
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oUserDao implements UserDao{
+    private Sql2o sql2o;
+
+    public Sql2oUserDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (firstName, lastName, email, mobile, password) VALUES (:firstName, :lastName, :email, :mobile, :password)";
+        try(Connection connection = sql2o.open()){
+            int id = (int) connection.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        }
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        try(Connection connection = sql2o.open()) {
+            return connection.createQuery("SELECT * FROM users")
+                    .executeAndFetch(User.class);
+        }
+    }
+}
